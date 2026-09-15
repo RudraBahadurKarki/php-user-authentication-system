9c6b612a232f89fe1d07996b5e6c198ba162de6c
Rudra Bahadur Karki <karkirudra856@gmail.com>
2026-09-15 21:36:20 +0545

    removed password

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
+
 $host = 'localhost';
 $user = 'root';
 $pass = '';
@@ -9,4 +10,5 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
